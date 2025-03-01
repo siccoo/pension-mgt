cdcb82c47802d3417d14acdd89f2f9440711c2f9
Chilaka Michael Obinna <obichilaka@gmail.com>
2025-03-02 00:10:53 +0100

    Setup & Installation of NPM Packages

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./index.css";
+
+const App: React.FC = () => {
+  return (
+    <h1 className='text-2xl text-blue-500'>Pension Management App</h1>
+  )
+}
+
+export default App
